9e0a45e8c955609cf5a43664615557c325cf6369
Oreoluwa <jetaneore22@gmail.com>
2024-04-12 00:50:20 +0100

    loyalty test

--- configdelegated.sh
+++ configdelegated.sh
@@ -8,7 +8,7 @@ echo -n "Input the stacksetname: "
 read stacksetname
 
 # Create a stackset
-aws cloudformation create-stack-set --stack-set-name $stacksetname --template-url https://cfntemplatesconfig.s3.amazonaws.com/EnableAWSConfigForOrganizations.yml --permission-model SERVICE_MANAGED --auto-deployment Enabled=true,RetainStacksOnAccountRemoval=true --call-as DELEGATED_ADMIN
+aws cloudformation create-stack-set --stack-set-name $stacksetname --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/EnableAWSConfig.yml --capabilities CAPABILITY_IAM
 
 # Get a list of all enabled regions in the organization
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text --region us-east-1)
@@ -33,6 +33,6 @@ read secondId
 # Enable AWS Config in all regions
 echo "Enabling AWS Config for regions: $json_array"
 
-aws cloudformation create-stack-instances --stack-set-name $stacksetname --deployment-targets OrganizationalUnitIds=["$firstId","$secondId"] --regions "$json_array" --operation-preferences FailureToleranceCount=7,MaxConcurrentCount=7,RegionConcurrencyType=PARALLEL --call-as DELEGATED_ADMIN
+aws cloudformation create-stack-instances --stack-set-name $stacksetname --accounts "["$firstId","$secondId"]" --regions "$json_array" --operation-preferences FailureToleranceCount=7,MaxConcurrentCount=7,RegionConcurrencyType=PARALLEL
 
 echo "AWS Config enabled across all regions in the organization."
