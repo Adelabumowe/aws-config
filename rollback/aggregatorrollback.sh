6fd746e107091664a6618a77fc1cc6d57868b4f0
Oreoluwa <jetaneore22@gmail.com>
2024-04-22 21:30:22 +0100

    new development

--- rollback/aggregatorrollback.sh
+++ rollback/aggregatorrollback.sh
@@ -5,30 +5,48 @@ root_arn=$(aws organizations list-roots --query "Roots[].Arn" --output text)
 org_id=$(echo "$root_arn" | cut -d'/' -f2)
 
 # Delete aggregator
-echo -n "Input the name of the aggregator to deleteLeave blank if you didn't create any: "
+echo -n "Input the name of the aggregator to delete(Leave blank if you didn't create any): "
 read aggregator
 
-echo -n "Input the region the aggregator and conformance pack was created in: "
+echo -n "Input the region the aggregator was created in: "
 read region
-
-# Location of conformance pack
-echo -n "Input the name for the conformance pack to delete(Leave blank if you didn't create any): "
-read conformancepack
-
-packname=$(aws configservice describe-organization-conformance-packs --query "OrganizationConformancePacks[?contains(OrganizationConformancePackName, '$conformancepack')].OrganizationConformancePackName" --region $region --output text)
 
 aws configservice delete-configuration-aggregator --configuration-aggregator-name $aggregator --region $region
 
-# Deploy the conformance pack
-aws configservice delete-organization-conformance-pack --organization-conformance-pack-name="$packname" --region $region
+# Conformance packs created
+echo -n "Input the name for the CIS conformance pack to delete(Leave blank if you didn't create any): "
+read cisconformancepackcheck
 
-# Delete delivery bucket (Bucket must start with awsconfigconforms)
-echo -n "Input the bucketname: "
-read bucketname
+echo -n "Input the name for the PCI-DSS conformance pack to delete(Leave blank if you didn't create any): "
+read pciconformancepackcheck
 
-aws s3api delete-bucket \
-    --bucket $bucketname
+cisconformancepack=$(aws configservice describe-organization-conformance-packs --query "OrganizationConformancePacks[?contains(OrganizationConformancePackName, '$cisconformancepackcheck')].OrganizationConformancePackName" --region $region --output text)
 
+pciconformancepack=$(aws configservice describe-organization-conformance-packs --query "OrganizationConformancePacks[?contains(OrganizationConformancePackName, '$pciconformancepackcheck')].OrganizationConformancePackName" --region $region --output text)
+
+# Get list of AWS regions
+regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text --region us-east-1)
+
+# Convert the regions into a JSON array
+json_array="["
+for region in $regions; do
+    json_array+="\"$region\","
+done
+json_array="${json_array%,}"  # Remove the trailing comma
+json_array+="]"
+
+# Loop through each region and run the AWS Config Service commands
+for region in $regions; do
+    echo "Deleting conformance packs in region: $region"
+
+    # Delete the Organization Conformance Pack for CIS
+    aws configservice delete-organization-conformance-pack --organization-conformance-pack-name="$cisconformancepack" --region $region
+
+    # Delete the Organization Conformance Pack for PCI DSS
+    aws configservice delete-organization-conformance-pack --organization-conformance-pack-name="$pciconformancepack" --region $region
+
+    echo "Completed processing region: $region"
+done
 
 echo -n "Input the aggregator policy arn: "
 read aggregatorpolicy
@@ -38,6 +56,13 @@ policyarn=$(aws iam list-policies --query 'Policies[?PolicyName=='$aggregatorpol
 
 # Create a policy for your AWS Config Aggregator
 aws iam delete-policy --policy-arn $policyarn
+
+# Delete delivery bucket (Bucket must start with awsconfigconforms)
+echo -n "Input the bucketname: "
+read bucketname
+
+aws s3api delete-bucket \
+    --bucket $bucketname
 
 # Delete an IAM role for your AWS Config aggregator
 
